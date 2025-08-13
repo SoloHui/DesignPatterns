df2fdc3bff3a54a45bde0e21f3ad74a91d6aca95
Jinghui Liang <3414467112@qq.com>
2025-08-13 16:43:31 +0800

    feat(simplefactory): 添加简单工厂示例代码

---
+++ chapter02/chapter02.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++17
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+  $$PWD/simplefactory.cpp \
+
+HEADERS += \
+  $$PWD/dynamicobject.h \
+  $$PWD/simplefactory.h
---
+++ chapter02/dynamicobject.h
@@ -0,0 +1,19 @@
+﻿/***************************************************************************
+  dynamicobject.h - %{Cpp:License:ClassName}
+
+ ---------------------
+ begin                : 13.8.2025
+ copyright            : (C) 2025 by Jinghui Liang
+ email                : 3414467112@qq.com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef DYNAMICOBJECT_H
+#define DYNAMICOBJECT_H
+
+#endif  // DYNAMICOBJECT_H
